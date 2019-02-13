4229450b73519ba79f3ca49ac6e28863aa226bb7
ryanboris <ryan.boris@gmail.com>
2019-02-13 02:57:52 -0500

    🎉 Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+      <span role="img" aria-label="emoji">
+        🚀 firecracker firecracker boom boom boom!
+      </span>
+    </>
+  )
+}

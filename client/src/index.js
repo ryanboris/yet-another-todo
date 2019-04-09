fb169e99b4731fded5fd764950383892ed4d8ebd
ryanboris <ryan.boris@gmail.com>
2019-04-09 01:31:33 -0400

    messing around

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,61 @@
-import React from 'react'
-import {render} from 'react-dom'
+import React, { useState, useReducer } from 'react'
+import { render } from 'react-dom'
+import uuidv4 from 'uuid'
 
-render(<div>start over</div>, document.getElementById('root') )
+const toDoContext = React.createContext()
+
+const reducer = ({ notes }, { type, payload, id }) => {
+  switch (type) {
+    case 'ADD':
+      return {
+        notes: [...notes, payload]
+      }
+    case 'DELETE':
+      return {
+        notes: notes.filter(n => n.id !== id)
+      }
+    case 'EDIT':
+
+    default:
+      return notes
+  }
+}
+
+const App = () => {
+  const [text, setText] = useState('')
+  const [toggle, setToggle] = useState(false)
+  const [state, dispatch] = useReducer(reducer, {
+    notes: [{ id: 0, text: 'test' }]
+  })
+
+  return (
+    <toDoContext.Provider value={{ state, dispatch }}>
+      <>
+        {state.notes.map(n => (
+          <div key={uuidv4()}>
+            <button onClick={() => setToggle(!toggle)}>edit</button>
+            <h3 onClick={() => dispatch({ type: 'DELETE', id: n.id })}>
+              {n.text}
+            </h3>
+          </div>
+        ))}
+        <form
+          onSubmit={e => {
+            e.preventDefault()
+            dispatch({ type: 'ADD', payload: { text, id: uuidv4() } })
+            setText('')
+          }}
+        >
+          <input
+            type="text"
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
+          <input type="submit" value="submit" />
+        </form>
+      </>
+    </toDoContext.Provider>
+  )
+}
+
+render(<App />, document.getElementById('root'))
